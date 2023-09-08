8be4cd39a51a9f186bb4a238be3b1daf4e3230b0
meritdominic200 <ugwuokedominic200@gmail.com>
2023-09-08 09:04:52 +0100

    first commit

---
+++ src/com/Chidera/Main.java
@@ -0,0 +1,26 @@
+package com.Chidera;
+
+public class Main {
+    public static void main(String[] args) {
+        int result = 2+4;
+        System.out.println(result);
+        result++;
+        System.out.println(result);
+        result = result%2;
+        System.out.println(result);
+        result*=10;
+        System.out.println(result);
+
+        boolean Myalien = false;
+        if(Myalien== true)
+            System.out.println("it is an alien");
+        else {
+            System.out.println("it is an alien");
+            result/=3;
+            System.out.println(result);
+
+
+        }
+
+    }
+}
---
+++ src/com/Chidera/Revision.java
@@ -0,0 +1,27 @@
+package com.Chidera;
+
+public class Revision {
+    public static void main(String[] args) {
+        int result = 4 + 5;
+        System.out.println("the result is " + result);
+        result+=4;
+        System.out.println("the result is " + result);
+        result = result - 5;
+        System.out.println("the result is " + result);
+        result -=1;
+        System.out.println("the result is " + result);
+        result *=8;
+        System.out.println("the result is " + result);
+        result/=2;
+        System.out.println("the result is " + result);
+        result%=8;
+        System.out.println("the result is " + result);
+        boolean Isboy = true;
+        if(Isboy==false)
+        System.out.println("this is an error");
+        else {
+                System.out.println("this is correct");
+        }
+
+    }
+}
